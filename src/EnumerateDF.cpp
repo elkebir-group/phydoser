9b15695931c2258258c9c87dd032ba18c86e045b
Leah Weber <leahlw2@ada.cs.illinois.edu>
2020-07-09 10:33:51 -0500

    removed timeout checkpoints

--- src/EnumerateDF.cpp
+++ src/EnumerateDF.cpp
@@ -17,7 +17,7 @@
 #include <sstream>
 #include <regex>
 #include <string.h>
-using namespace Rcpp;
+//using namespace Rcpp;
 
 typedef std::map<std::string,std::string> StringMap;
 typedef std::map<std::string,int> StringIntMap;
@@ -46,13 +46,16 @@ bool is_cover(Feature pi, Family& Fam, Subset Universe){
 
 void getDFF(Family& fam, Subset U, int m, FeatureFamily& Phi, FeatureFamily& allFeatures){
     //  FeatureFamily Phi;
+    
     for (int i = 0; i < allFeatures.size(); ++i){
         Feature afeature = allFeatures[i];
+      
         bool c = is_cover(afeature, fam, U);
         if (c){
             bool to_add = true;
             for (int j = 0; j < Phi.size(); ++j){
                 Feature feature = Phi[j];
+              
                 if (std::includes(afeature.begin(),afeature.end(),feature.begin(),feature.end())){
                     to_add = false;
                     break;
@@ -89,11 +92,12 @@ Rcpp::List enumerateDF(Rcpp::List tree_mats){
     //          - Each row is added to an intsetset (intsetset represents each matrix)
     std::vector<IntSetSet> treesFeatures(nrTrees);
     for (int i = 0; i < nrTrees; i++){ // for each tree
-       Rcpp::checkUserInterrupt();
         std::vector<std::string> mutsList = Rcpp::as<std::vector<std::string>>(Rcpp::colnames(tree_mats[i])); // each tree may have different order of colnames
         for (int j = 0; j < nrMuts; j++){ // for each row (clone/feature)
             IntSet feature;
+          
             for (int c = 0; c < nrMuts; c++){ // for each column (mutation)
+                
                 Rcpp::NumericMatrix mat = tree_mats[i];
                 double elem = mat(j,c);
                 if (elem == 1){
@@ -108,16 +112,19 @@ Rcpp::List enumerateDF(Rcpp::List tree_mats){
     // Step 3. Enumerate all sets of size 1<= i <= m containing combinations of integers from 1 to m. Each combination of integers is called "Feature"
     FeatureFamily allFeatures;
     for (int i = 1; i <= m; ++i){
-      Rcpp::checkUserInterrupt();
+      
         Feature new_feature;
         for (int j = 1; j <= i; ++j){
+          
             new_feature.push_back(j);
         }
         Feature add_feature2(new_feature);
         allFeatures.push_back(add_feature2);
         while(true){
+          
             bool nothing_new = true;
             for (int idx = i-1; idx >= 0; --idx){
+              
                 if (new_feature[idx] < m && (idx == i-1 || new_feature[idx] < new_feature[idx+1]-1)){
                     new_feature[idx]+= 1;
                     int inc = new_feature[idx]+1;
@@ -145,16 +152,17 @@ Rcpp::List enumerateDF(Rcpp::List tree_mats){
     std::vector<Family> FamilyVector2;
  
     for (int i = 0; i < nrTrees; ++i){
-      Rcpp::checkUserInterrupt();
+      
         Family fam; // Feature family of tree i (a collection of features)
         IntSetVector itfMap2(m);
         int idx = 0;
         for (auto it = treesFeatures[i].begin(); it != treesFeatures[i].end(); ++it){
             // For every mutation set (feature) of tree i, check if other trees have that feature
             Subset feat; // One feature of tree i
-            
+          
             itfMap2[idx++] = *it;
             for (int j = 0; j < nrTrees; ++j){
+              
                 if (i == j){
                     feat[j] = 1;
                 }
@@ -171,13 +179,14 @@ Rcpp::List enumerateDF(Rcpp::List tree_mats){
     // Step 5. Define the universe
     Subset Universe;
     for (int i = 0; i < nrTrees; ++i){
+      
         Universe[i] = 1;
     }
     
     // Step 6. Enumerate distingushing features for each tree
     std::vector<FeatureFamily> PhiVector;
     for (auto fam : FamilyVector2){
-        Rcpp::checkUserInterrupt();
+        
         FeatureFamily Phi;
         getDFF(fam, Universe, m, Phi, allFeatures);
         PhiVector.push_back(Phi);
@@ -186,10 +195,13 @@ Rcpp::List enumerateDF(Rcpp::List tree_mats){
     // Step 5. Convert distinguishing features to R List
     Rcpp::List ret = Rcpp::List::create();
     for (int i = 0; i < nrTrees; ++i){
+      
         Rcpp::List treeDFF = Rcpp::List::create();
         for (auto Feature : PhiVector[i]){
             std::string featureStr = "";
+          
             for (int mutIdx : Feature){
+              
                 for (auto mut : idxToFeatureMap2[i][mutIdx-1]){
                     featureStr += idToMut[mut];
                     featureStr += " ";
